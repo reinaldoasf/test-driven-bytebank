e455baca3fd187cbd062a58f67d5a1343184a05e
reinaldoasf <reinaldoasf@gmail.com>
2024-03-10 18:39:16 -0300

    added custom markers on the tests

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,5 +1,6 @@
 from codigo.bytebank import Funcionario
 import pytest
+from pytest import mark
 
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_24(self):
@@ -34,6 +35,7 @@ class TestClass:
 
         assert  resultado == esperado #THEN
 
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
         entrada = 1000 #given
         esperado = 100
@@ -43,7 +45,7 @@ class TestClass:
 
         assert resultado == esperado #then
 
-
+    @mark.calcular_bonus
     def test_quando_calcular_bonus_recebe_1000000000_deve_retornar_exception(self):
         with pytest.raises(Exception):
             entrada = 1000000000  # given
